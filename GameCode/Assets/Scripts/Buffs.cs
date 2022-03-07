ad844133eb6985ffd1e95064a2a22731ee2617f0
GOSUwat <78549782+GOSUwat@users.noreply.github.com>
2022-03-07 16:52:44 +0300

    yyyy

--- GameCode/Assets/Scripts/Buffs.cs
+++ GameCode/Assets/Scripts/Buffs.cs
@@ -12,6 +12,7 @@ public class Buffs : MonoBehaviour
     private float buffid = 0;
     private float buffDur = 0;
     private float speed = 0;
+    private string BuffType;
 
 
 
@@ -36,7 +37,8 @@ public class Buffs : MonoBehaviour
             buffDur = _buffSpeed.buffDuration;
             buffid = _buffSpeed.buffID;
             speed = _movement.movementSpeed;
-            OnBuff(_buffSpeed.buffID);
+
+            OnBuff(buffid);
                
             Destroy(collision.gameObject);
         }
@@ -45,7 +47,7 @@ public class Buffs : MonoBehaviour
     private void OnGUI()
     {
             GUI.Label(new Rect(500, 15, 75, 75), "Buff time Left is " + buffDur.ToString());
-            GUI.Label(new Rect(700, 15, 75, 75), "Buff type is " + buffid.ToString());
+            GUI.Label(new Rect(700, 15, 75, 75), "Buff type is " + BuffType);
     }
 
     private void Update()
@@ -56,11 +58,22 @@ public class Buffs : MonoBehaviour
             SpeedChange(buffDur);
         }
 
+            switch (buffid)
+        {
+            case 1:
+                BuffType = "Speed";
+                break;
+            case 2:
+                BuffType = "+ HEALTH";
+                break;
+            case 3:
+                BuffType = "Spike Shield";
+                break;
+        }
+
         
     }
 
-    //1 speed
-    //2 +hp
     void OnBuff(int x)
     {
          if (buffid == 1)
@@ -72,13 +85,15 @@ public class Buffs : MonoBehaviour
 
          }
 
-            else if (buffid == 2)
-            {
+         else if (buffid == 2)
+         {
                 hp.hp += 2;
                 buffDur = 0;
-            } 
+         } 
 
+         else if (buff) { }
     }
+
     void SpeedChange(float time)
     {
         if(time <= 0)
